b44cfcd38db9b569df3275560f12782318eff8c9
Christopher Mancini <cmancini@basho.com>
2017-03-30 10:31:05 -0400

    Initial app development.

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+
+header('Content-Type: application/json');
+$response = [
+        'success' => true,
+        'message' => 'CircleCI',
+    ];
+
+if (empty($_POST['button_clicked'])) {
+    $response['success'] = false;
+    $response['message'] = 'Invalid request.';
+}
+
+print(json_encode($response));
